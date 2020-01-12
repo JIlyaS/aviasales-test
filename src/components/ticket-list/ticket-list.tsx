dd163689d8ac34dfebd0cfacdc83ef5181bbafc2
JIlyaS <ilkolmakov@yandex.ru>
2020-01-13 00:10:47 +0500

    3 add loading and add error block

--- src/components/ticket-list/ticket-list.tsx
+++ src/components/ticket-list/ticket-list.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {observer, inject} from 'mobx-react';
 
+import LoadingBlock from '../loading-block/loading-block';
+import ErrorBlock from '../error-block/error-block';
 import TicketItem from '../ticket-item/ticket-item';
 import styles from './ticket-list.module.scss';
 
-const TicketList = inject('ticketStore')(observer(({ticketStore}) => {
-
-  if (!ticketStore.tickets.length) {
-    return <div>Loading...</div>;
+const TicketList = inject('ticketStore')(observer(({ticketStore: {isLoadTicketError, tickets}}) => {
+  
+  if (isLoadTicketError) {
+    return <ErrorBlock />;
   }
+
+  if (!tickets.length) {
+    return <LoadingBlock />;
+  }
+
   return (
     <div className={styles.ticketList}>
       {
-       ticketStore.tickets.map((ticket: Array<object> | any, index: number) => <TicketItem key={index} {...ticket} />) 
+       tickets.map((ticket: Array<object> | any, index: number) => <TicketItem key={index} {...ticket} />) 
       }
     </div>
   );
--- src/stores/ticket-store.tsx
+++ src/stores/ticket-store.tsx
@@ -12,26 +12,36 @@ class TicketStore {
 
   tickets: Array<object> | any = [];
   isInfoActive: Boolean = false;
+  isLoadTicketError: Boolean = false;
 
   loadTickets() {
-    api.get('/tickets').then((response) => {
-      if (response.data) {
-        runInAction(() => {
-          this.tickets = response.data.slice(0, 5).sort((prev: {price: any}, curr: {price: any}) => {
-            return prev.price - curr.price;
+    setTimeout(() => {
+      api.get('/tickets').then((response) => {
+        if (response.data) {
+          runInAction(() => {
+            this.tickets = response.data.slice(0, 5).sort((prev: {price: any}, curr: {price: any}) => {
+              return prev.price - curr.price;
+            });
           });
-        });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+        }
+      })
+      .catch((error) => {
+        this.setLoadTicketError();
+        console.log(error);
+      });
+    }, 2000);
+  }
+
+  setLoadTicketError() {
+    this.isLoadTicketError = true;
   }
 }
 
 decorate(TicketStore, {
   loadTickets: action.bound,
+  setLoadTicketError: action.bound,
   tickets: observable.shallow,
+  isLoadTicketError: observable
 });
 
 export default TicketStore;
